b78a3cf9669b26c9ae6df3f12e28a6b7903914ab
daveawb <david.aw.barker@gmail.com>
2015-03-07 13:20:00 +0000

    added watchify feature
    added in multi bundles
    
    Update index.js
    
    removed console.log
    
    refactored multi bundle approach
    
    refactored multi bundle approach
    
    refactored multi bundle approach
    
    added docblock

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     elixir = require('laravel-elixir'),
+    config = elixir.config,
+    inSequence = require('run-sequence'),
     utilities = require('laravel-elixir/ingredients/commands/Utilities'),
     notifications = require('laravel-elixir/ingredients/commands/Notification'),
     gulpIf = require('gulp-if'),
@@ -8,46 +10,94 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
     browserify = require('browserify'),
+    watchify = require('watchify'),
     _  = require('underscore');
 
-elixir.extend('browserify', function (src, options) {
+/**
+ * Create the Gulp task.
+ *
+ * @return {void}
+ */
+var buildTask = function() {
+    var stream;
 
-    var config = this,
-        defaultOptions = {
-            debug:         ! config.production,
-            rename:        null,
-            srcDir:        config.assetsDir + 'js',
-            output:        config.jsOutput,
-            transform:     [],
-            insertGlobals: false,
-        };
-
-    options = _.extend(defaultOptions, options);
-    src = "./" + utilities.buildGulpSrc(src, options.srcDir);
-
-    gulp.task('browserify', function () {
+    gulp.task('browserify', function(callback) {
 
         var onError = function(e) {
             new notifications().error(e, 'Browserify Compilation Failed!');
             this.emit('end');
         };
 
-        var browserified = function(filename) {
-            var b = browserify(filename, options);
-            
-            return b.bundle();
-        };
+        var bundle = function(b, instance) {
+            return b.bundle()
+                .on('error', onError)
+                .pipe(source(instance.src.split("/").pop()))
+                .pipe(buffer())
+                .pipe(gulpIf(!instance.options.debug, uglify()))
+                .pipe(gulpIf(typeof instance.options.rename === 'string', rename(instance.options.rename)))
+                .pipe(gulp.dest(instance.options.output))
+                .pipe(new notifications().message('Browserified!'));
+        }
 
-        return browserified(src).on('error', onError)
-            .pipe(source(src.split("/").pop()))
-            .pipe(buffer())
-            .pipe(gulpIf(! options.debug, uglify()))
-            .pipe(gulpIf(typeof options.rename === 'string', rename(options.rename)))
-            .pipe(gulp.dest(options.output))
-            .pipe(new notifications().message('Browserified!'));
+        config.toBrowserify.forEach(function(instance) {
+            var b = browserify(instance.src, instance.options);
+
+            if (config.watchify) {
+                b = watchify(b);
+
+                b.on('update', function() {
+                    bundle(b, instance);
+                });
+            }
+
+            stream = bundle(b, instance);
+        });
+
+        return stream;
+    });
+}
+
+/**
+ * Create elixir extension
+ */
+elixir.extend('browserify', function (src, options) {
+
+    if (!_.isArray(config.toBrowserify))
+        config.toBrowserify = [];
+
+    options = _.extend({
+        debug:         ! config.production,
+        rename:        null,
+        srcDir:        config.assetsDir + 'js',
+        output:        config.jsOutput,
+        transform:     [],
+        insertGlobals: false
+    }, options);
+
+    config.toBrowserify.push({
+        src : "./" + utilities.buildGulpSrc(src, options.srcDir),
+        options: options
     });
 
-    this.registerWatcher('browserify', options.srcDir + '/**/*.js');
+    buildTask();
 
-    return this.queueTask('browserify');
+    this.registerWatcher('browserify', options.srcDir + '/**/*.js', config.watchify ? 'nowatch' : 'default');
+
+    return config.queueTask('browserify');
 });
+
+/**
+ * Create elixir extension for Watchify command
+ */
+elixir.extend('watchify', function(src, options) {
+
+    var config = this;
+
+    gulp.task('watchify', ['watch'], function() {
+        config.watchify = true;
+
+        inSequence.apply(this, ['browserify']);
+    });
+
+    return this.queueTask('watchify');
+})
